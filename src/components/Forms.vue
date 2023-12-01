89a912445cd6e3d81ec4bf1f438253b2f8d3c25e
Nilesh Shinde <nilesh.shinde@angularminds.in>
2023-12-01 10:46:46 +0530

    update

--- src/components/Forms.vue
+++ src/components/Forms.vue
@@ -1,71 +1,84 @@
 <template lang="">
-    <div>
+    <div class='outerContainer'>
        <form @submit.prevent="submitForm">
     <!-- input  -->    
-    <label for="name">Name:</label>
-    <input type="text" id="name" v-model="formData.name" @change="validateName">
-    <span v-if="!nameValid && submitted" style="color: red;">Please enter a valid name</span><br>
+    <div>
+       <label for="name">Name:</label>
+        <input type="text" id="name" v-model="formData.name" @input="validateName">
+    </div>
+    <span v-if="!nameValid && submitted" class="warning-font-size" style="color: red;">Please enter a valid name</span><br>
 
     <!-- email -->
-    <label for="email">Email:</label>
-    <input type="email" id="email" v-model="formData.email" @change="validateEmail">
-    <span v-if="!emailValid && submitted" style="color: red;">Please enter a valid email</span><br>
+    <div>
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="formData.email" @input="validateEmail">
+    </div>
+    <span v-if="!emailValid && submitted" style="color: red;"  class="warning-font-size">Please enter a valid email</span><br>
 
     <!-- mobile -->
-    <label for="mobile">Mobile No:</label>
-    <input type="tel" id="mobile" v-model="formData.mobile" @change="validateMobile">
-    <span v-if="!mobileValid && submitted" style="color: red;">Please enter a valid mobile number</span><br>
+   <div>
+       <label for="mobile">Mobile No:</label>
+       <input type="tel" id="mobile" v-model="formData.mobile" @input="validateMobile">
+   </div>
+    <span v-if="!mobileValid && submitted" style="color: red;"  class="warning-font-size">Please enter a valid mobile number</span><br>
 
     <!-- select box -->
-    <label for="occupation">Occupation:</label>
-    <select id="occupation" v-model="formData.occupation">
-      <option value="developer">Developer</option>
-      <option value="designer">Designer</option>
-      <option value="manager">Manager</option>
-    </select>
-    <span v-if="!mobileValid && submitted" style="color: red;">Please enter a valid mobile number</span><br>
+    <div>
+          <label for="occupation">Occupation:</label>
+          <select id="occupation" v-model="formData.occupation" @change="validateOccupation">
+            <option value="developer">Developer</option>
+            <option value="designer">Designer</option>
+            <option value="manager">Manager</option>
+          </select>
+    </div>
+    <span v-if="!occupationValid && submitted" style="color: red;"  class="warning-font-size">Please enter a valid occupation</span><br>
 
     <!-- radio box -->
-    <label>Gender:</label>
-    <label for="male">Male</label>
-    <input type="radio" id="male" value="male" v-model="formData.gender" @change="validateGender">
-    <label for="female">Female</label>
-    <input type="radio" id="female" value="female" v-model="formData.gender" @change="validateGender">
-    <span v-if="!genderValid && submitted" style="color: red;">Please select a gender</span><br>
+    <div>
+          <label>Gender:</label>
+          <input type="radio" id="male" value="male" v-model="formData.gender" @change="validateGender">
+          <label for="male">Male</label>
+          <input type="radio" id="female" value="female" v-model="formData.gender" @change="validateGender">
+          <label for="female">Female</label>
+    </div>
+    <span v-if="!genderValid && submitted" style="color: red;"  class="warning-font-size">Please select a gender</span><br>
 
     <!-- check box -->
-    <label>Hobbies:</label>
-    <label for="hobby1">Reading</label>
-    <input type="checkbox" id="hobby1" value="reading" v-model="formData.hobbies" @change="validateHobbies">
-    <label for="hobby2">Gaming</label>
-    <input type="checkbox" id="hobby2" value="gaming" v-model="formData.hobbies" @change="validateHobbies">
-    <label for="hobby3">Traveling</label>
-    <input type="checkbox" id="hobby3" value="traveling" v-model="formData.hobbies" @change="validateHobbies">
-    <span v-if="formData.hobbies.length < 1 && submitted" style="color: red;">Please select at least one hobby</span><br>
+   <div>
+           <label>Hobbies:</label>
+          <label for="hobby1">Reading</label>
+          <input type="checkbox" id="hobby1" value="reading" v-model="formData.hobbies" @input="validateHobbies">
+          <label for="hobby2">Gaming</label>
+          <input type="checkbox" id="hobby2" value="gaming" v-model="formData.hobbies" @input="validateHobbies">
+          <label for="hobby3">Traveling</label>
+          <input type="checkbox" id="hobby3" value="traveling" v-model="formData.hobbies" @input="validateHobbies">
+   </div>
+    <span v-if="formData.hobbies.length < 1 && submitted" style="color: red;"  class="warning-font-size">Please select at least one hobby</span><br>
 
     <!-- range -->
-    <label for="feedback">Feedback (1-10):</label>
-    <input type="range" id="feedback" v-model="formData.feedback" min="0" max="10" @change="validateFeedback">
-    <span v-if="!feedbackValid && submitted" style="color: red;">Please select a valid feedback score between 1 and 10</span><br>
+    <div>
+          <label for="feedback">Feedback (1-10):</label>
+          <input type="range" id="feedback" v-model="formData.feedback" min="0" max="10" @input="validateFeedback">
+    </div>
+    <span v-if="!feedbackValid && submitted" style="color: red;"  class="warning-font-size">Please select a valid feedback score between 1 and 10</span><br><br>
 
     <!-- experiance -->
-    <div id="app">
-    <div v-for="(experience, index) in experiences" :key="index">
-      <label for="companyName">Company Name:</label>
-      <input type="text" v-model="experience.companyName" @change='validExperiance'>
-      <span v-if="!experience.companyNameValid && submitted" style="color: red;">Please enter a valid name</span><br>
+      <span>Experience: </span>
+      <div v-for="(experience, index) in formData.experiences" :key="index" class="experience" >
+        <label for="companyName">Company Name:</label>
+        <input type="text" v-model="experience.companyName" @input='validExperiance'><br>
+        <span v-if="!experience.companyNameValid && submitted" style="color: red;"  class="warning-font-size">Please enter a valid name</span><br>
 
-      <label for="experience">Experience:</label>
-      <input type="text" v-model="experience.experience" @change="validExperiance">
+        <label for="experience">Experience:</label>
+        <input type="text" v-model="experience.experience" @input="validExperiance"><br>
 
-      <button @click="removeExperience(index)">Remove</button>
-      <span v-if="!experience.experienceValid && submitted" style="color: red;">Please enter a valid name</span><br>
-    </div>
+        <span v-if="!experience.experienceValid && submitted" style="color: red;"  class="warning-font-size">Please enter a valid name</span><br>
+        <button @click="removeExperience(index)">Remove</button>
+      </div>
 
-    <button @click="addExperience">Add Experience</button>
+    <button type='button' @click="addExperience">Add Experience</button>
     <button @click='submitExperiace' >Submit experiance</button>
 
-  </div>
 
     <button type="submit" >Submit</button>
   </form>
@@ -84,6 +97,14 @@ export default {
         gender: "",
         hobbies: [],
         feedback: 0,
+        experiences: [
+          {
+            companyName: "",
+            experience: "",
+            companyNameValid: true,
+            experienceValid: true,
+          },
+        ],
       },
       nameValid: true,
       emailValid: true,
@@ -94,30 +115,31 @@ export default {
       feedbackValid: true,
       formIsValid: false,
       submitted: false,
-      experiancesValid: true,
-      experiences: [
-        {
-          companyName: "",
-          experience: "",
-          companyNameValid: true,
-          experienceValid: true,
-        },
-      ],
     };
   },
 
-  //   watch: {
-  //     formData: {
-  //       handler(newValue, oldValue) {
-  //         console.log("deepObject changed:", newValue, oldValue);
-  //       },
-  //       deep: true, // Set deep to true for deep watching
+  // watch: {
+  //   formData: {
+  //     handler(oldVal, newVal) {
+  //       console.log(oldVal, newVal);
+  //       if (oldVal != newVal) {
+  //         this.validateName();
+  //         this.validateEmail();
+  //         this.validateMobile();
+  //         this.validateGender();
+  //         this.validateHobbies();
+  //         this.validateFeedback();
+  //         this.validateOccupation();
+  //         this.validExperiance();
+  //       }
   //     },
+  //     deep: true, // Set deep to true for deep watching
   //   },
+  // },
+
   methods: {
     submitExperiace() {
-      console.log(this.experiancesValid);
-      console.log(this.experiences);
+      console.log(this.formData.experiences);
     },
     submitForm() {
       this.submitted = true;
@@ -128,6 +150,7 @@ export default {
       this.validateHobbies();
       this.validateFeedback();
       this.validateOccupation();
+      this.validExperiance();
 
       this.formIsValid =
         this.nameValid &&
@@ -135,7 +158,11 @@ export default {
         this.mobileValid &&
         this.genderValid &&
         this.hobbiesValid &&
-        this.feedbackValid;
+        this.feedbackValid &&
+        this.occupationValid &&
+        this.formData.experiences?.every(
+          (item) => item.companyName != "" && item.experience != ""
+        );
 
       if (this.formIsValid) {
         console.log("Form submitted:", this.formData);
@@ -170,27 +197,34 @@ export default {
         this.formData.feedback >= 1 && this.formData.feedback <= 10;
     },
     addExperience() {
-      this.experiences.push({ companyName: "", experience: "" });
+      this.formData.experiences.push({ companyName: "", experience: "" });
     },
     removeExperience(index) {
-      this.experiences.splice(index, 1);
+      this.formData.experiences.splice(index, 1);
     },
     validExperiance() {
-      // this.experiancesValid = this.experiences.every((experience) => {
-      //   return (
-      //     experience.companyName.trim() !== "" &&
-      //     experience.experience.trim() !== ""
-      //   );
-      // });
-      this.experiences = this.experiences.map((item) => ({
+      this.formData.experiences = this.formData.experiences.map((item) => ({
         ...item,
         companyNameValid: item.companyName.trim() !== "" ? true : false,
         experienceValid: item.experience.trim() !== "" ? true : false,
       }));
-      console.log(this.experiences);
+      console.log(this.formData.experiences);
     },
   },
 };
 </script>
 <style >
+.outerContainer {
+  margin-left: 30%;
+}
+.warning-font-size {
+  font-size: 12px;
+}
+.experience {
+  border: 1px solid black !important;
+  padding: 10px;
+  margin: 5px;
+  width: 350px;
+  border-radius: 5px;
+}
 </style>
